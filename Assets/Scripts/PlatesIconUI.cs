0c6d6f20e071d550b1c8bdc6624f86aa02c1abc4
laila-chammaa <laila.chammaa@unity3d.com>
2025-12-30 10:58:23 -0500

    Add plate UI icons

---
+++ Assets/Scripts/PlatesIconUI.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class PlatesIconUI : MonoBehaviour
+{
+    [SerializeField]
+    PlateKitchenObject plateKitchenObject;
+
+    [SerializeField]
+    Transform iconTemplate;
+
+    void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObjectOnOnIngredientAdded;
+    }
+
+    void PlateKitchenObjectOnOnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (Transform child in transform)
+        {
+            // Leave the first template
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (var kitchenObjectSo in plateKitchenObject.GetKitchenObjectSOList())
+        {
+            // Create an iconTemplate with that icon
+            var icon = Instantiate(iconTemplate, transform);
+            icon.gameObject.SetActive(true);
+            icon.GetComponent<PlateIconTemplate>().SetIconImage(kitchenObjectSo.sprite);
+        }
+    }
+
+}
