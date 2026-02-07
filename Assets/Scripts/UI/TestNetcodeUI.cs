236f92decd8e22400e5a84989ae41c03ee14f1f9
laila-chammaa <laila.chammaa@unity3d.com>
2026-02-07 13:39:37 -0500

    Move to LTS

--- Assets/Scripts/UI/TestNetcodeUI.cs
+++ Assets/Scripts/UI/TestNetcodeUI.cs
@@ -3,6 +3,36 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+[RequireComponent(typeof(UIDocument))]
+public class TestNetcodeUI : MonoBehaviour
+{
+    void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var hostButton = root.Q<Button>("host");
+        var clientButton = root.Q<Button>("client");
+
+        hostButton.clicked += () =>
+        {
+            NetworkManager.Singleton.StartHost();
+            Hide();
+        };
+
+        clientButton.clicked += () =>
+        {
+            NetworkManager.Singleton.StartClient();
+            Hide();
+        };
+    }
+
+    void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
+
+// Tested new features in 6000.5, worked great, but I want to work on the LTS
+/*
 [RequireComponent(typeof(PanelRenderer))]
 public class TestNetcodeUI : MonoBehaviour
 {
@@ -48,3 +78,4 @@ public class TestNetcodeUI : MonoBehaviour
         gameObject.SetActive(false);
     }
 }
+*/
